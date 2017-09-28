752dd5cd8ca076c6bb2c7a5508b9aecf747fca7c
Christian Barbato <christian.barbato@gpi.it>
2017-09-28 15:52:52 +0200

    First commit

---
+++ src/main/java/it/christianb/pfinanceblocking/PfinanceBlockingApplication.java
@@ -0,0 +1,12 @@
+package it.christianb.pfinanceblocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfinanceBlockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfinanceBlockingApplication.class, args);
+	}
+}
